d4399cc53d0ead8b386c97569733018c765f8a30
88plug <19512127+88plug@users.noreply.github.com>
2021-02-26 14:15:07 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+docker build -t storj_setup .
+docker run -itd --name storj_setup storj_setup
+docker cp storj_setup:/root/.local/share/storj/identity .
+
+mkdir storj
+
+docker run --rm -e SETUP="true" \
+    --mount type=bind,source=/root/storj-docker/identity/storagenode,destination=/app/identity \
+    --mount type=bind,source=/root/storj-docker/storj/,destination=/app/config \
+    --name storagenode storjlabs/storagenode:latest
+
+docker run -d --restart unless-stopped --stop-timeout 300 \
+    -p 28967:28967 \
+    -p 127.0.0.1:14002:14002 \
+    -e WALLET="0xxxx" \
+    -e EMAIL="x@x.com" \
+    -e ADDRESS="x.x.x.x:28967" \
+    -e STORAGE="10GB" \
+    --mount type=bind,source=/root/storj-docker/identity/storagenode,destination=/app/identity \
+    --mount type=bind,source=/root/storj-docker/storj/,destination=/app/config \
+    --name storagenode storjlabs/storagenode:latest
